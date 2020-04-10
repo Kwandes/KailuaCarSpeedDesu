3d26bcdf41979d86d3efd4c2d779769c4508346f
Alex <nyt604@gmail.com>
2020-04-10 15:07:09 +0200

    add formattedPrint, formattedHeader, printLines and fix regions

--- src/main/java/Queries.java
+++ src/main/java/Queries.java
@@ -1,28 +1,58 @@
+import java.security.spec.RSAOtherPrimeInfo;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
-//region System.out.prints
-
-
-
-//endregion
-
-//region queries
 public class Queries {
 
+    private static final String art = "☭";
+    private static final int headerLinesCount = 100;
+    private static final String headerLines = "*";
+
+    //region System.out.prints
+
+
+    public static void formattedPrint(String text)
+    {
+        System.out.printf("%-4s%-95s%-4s%n", art, text, art);
+    }
+
+    public static void formattedHeader(String text)
+    {
+        printLines();
+        if (text.length()%2 != 0)
+        {
+            text += " ";
+        }
+        int numbers = (headerLinesCount-text.length())/2;
+        System.out.printf("%-" + numbers + "s%s%" + numbers + "s%n", art, text, art);
+        printLines();
+        System.out.println();
+        System.out.println();
+    }
+
+    private static void printLines()
+    {
+        for (int i=0; i<=headerLinesCount; i++)
+        {
+            System.out.print(headerLines);
+        }
+        System.out.println();
+    }
+    //endregion
+
+    //region queries
     public static void addPerson(String customer)
     {
         String DBCostumorInfo = "INSERT INTO constumer( " +
                 "first_name, last_name, cpr, zip, city, address, phone_number, email, drivers_licence_number, driver_since_date " +
                 ") VALUES ( " +
                 customer + " );";
-        DBInteraction.updateData( DBCostumorInfo );
+        DBInteraction.updateData(DBCostumorInfo);
     }
 
-    public static void addRental( Date startDate, Date endDate, int costumerId, int salesmanId, int carId, int maxKm, int startKm)
+    public static void addRental(Date startDate, Date endDate, int costumerId, int salesmanId, int carId, int maxKm, int startKm)
     {
-        SimpleDateFormat sdf = new SimpleDateFormat( "dd-MM-yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         String DBContractInfo = "INSERT INTO Contract( " +
                 "date_signed, " +
                 "end_date, " +
@@ -33,10 +63,10 @@ public class Queries {
                 "start_km," +
                 "value" +
                 ") VALUES ( " +
-                "'" + sdf.format( startDate ) + "', '" + sdf.format( endDate ) + "', '" + costumerId + "', '" + salesmanId + "', '" + carId +
+                "'" + sdf.format(startDate) + "', '" + sdf.format(endDate) + "', '" + costumerId + "', '" + salesmanId + "', '" + carId +
                 "', '" + maxKm + "', '" + startKm + "'";
-        DBInteraction.updateData( DBContractInfo );
+        DBInteraction.updateData(DBContractInfo);
     }
-//endregion
-    
+    //endregion
 }
+
